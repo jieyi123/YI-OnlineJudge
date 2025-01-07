7bb7db5eb1e2b95526df57ca60f4aa9070fe6b39
jieyi123 <pjieyi@foxmail.com>
2025-01-07 13:26:50 +0800

    前后端项目初始化

---
+++ yioj-backend/src/main/java/com/pjieyi/yioj/constant/FileConstant.java
@@ -0,0 +1,16 @@
+package com.pjieyi.yioj.constant;
+
+/**
+ * 文件常量
+ *
+
+ */
+public interface FileConstant {
+
+    /**
+     * COS 访问地址
+     * todo 需替换配置
+     */
+    String COS_HOST = "腾讯云对象地址";
+    String OSS_HOST = "阿里云对象地址";
+}
---
+++ yioj-backend/src/main/java/com/pjieyi/yioj/model/dto/postfavour/PostFavourQueryRequest.java
@@ -0,0 +1,29 @@
+package com.pjieyi.yioj.model.dto.postfavour;
+
+import com.pjieyi.yioj.common.PageRequest;
+import com.pjieyi.yioj.model.dto.post.PostQueryRequest;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 帖子收藏查询请求
+ *
+
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class PostFavourQueryRequest extends PageRequest implements Serializable {
+
+    /**
+     * 帖子查询请求
+     */
+    private PostQueryRequest postQueryRequest;
+
+    /**
+     * 用户 id
+     */
+    private Long userId;
+
+    private static final long serialVersionUID = 1L;
+}
